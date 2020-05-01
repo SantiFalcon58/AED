5bee1d50d93261b4148a1a94e57a376d63ccdf71
SantiFalcon58 <64263012+SantiFalcon58@users.noreply.github.com>
2020-05-01 19:42:11 -0300

    Create Adicion.cpp

---
+++ DD-Adicion/Adicion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout << "Ingrese el primer numero\n";
+    cin >> a;
+    cout << "Ingrese el segundo numero\n";
+    cin >> b;
+    int suma = a+b;
+     cout << "La suma es: \n";
+     cout << suma;
+    return 0;
+}
